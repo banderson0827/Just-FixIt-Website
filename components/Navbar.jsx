eacbab4bed265748e87ad618f3b768275ec76e21
Brett Anderson <banderson0827@hotmail.com>
2023-11-09 19:37:33 -0500

    Website successfully deployed on Hostinger.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { Link } from 'next/link';
+import Link from 'next/link';
 
 import styles from '../styles';
 import { navLinks } from '../constants';
